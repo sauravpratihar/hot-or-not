65c784d3f3bd97c083851f572143ad7d2e18f718
Saurav Pratihar <pratihar.saurav@gmail.com>
2017-07-31 00:41:24 +0530

    mongo add

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
 
     'secret': 'sauravpratihar',
-    'database': 'mongodb://localhost:27017/hotornot'
+    'database': process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost:27017/hotornot'
 
 };
